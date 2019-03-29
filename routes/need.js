9a6f89ed1aa40b2ebb4ac15b8d3697721cca2436
franlol <fraaaaan@gmail.com>
2019-03-30 00:04:13 +0100

    Post route for edit needs (/need/id) done and tested

--- routes/need.js
+++ routes/need.js
@@ -7,6 +7,7 @@ const User = require('../models/User');
 
 const { isLoggedIn, isNotLoggedIn, validationLoggin } = require('../helpers/middlewares');
 
+// New one
 router.post('/add', isLoggedIn(), async (req, res, next) => {
     const { id, title, rate, description } = req.body;
 
@@ -36,6 +37,7 @@ router.post('/add', isLoggedIn(), async (req, res, next) => {
 
 });
 
+// getting last 20
 router.get('/latest', isLoggedIn(), async (req, res, next) => {
     try {
         const latest = await Need.find({ isActive: true }).sort({ 'created_at': -1 }).limit(20);
@@ -51,7 +53,8 @@ router.get('/latest', isLoggedIn(), async (req, res, next) => {
 
 });
 
-router.get('/:id', async (req, res, next) => {
+// get by id
+router.get('/:id', isLoggedIn(), async (req, res, next) => {
     const { id } = req.params;
 
     if (!id || !mongoose.Types.ObjectId.isValid(id)) {
@@ -77,10 +80,52 @@ router.get('/:id', async (req, res, next) => {
 
 });
 
-router.post('/apply/:id', (req, res, next) => {
+// edit need
+router.post('/:id', isLoggedIn(), async (req, res, next) => {
+    const { userId } = req.body;
+    const { needId, title, rate, description } = req.body.need;
 
-    console.log(req.body)
+    if (!userId || !needId || !title || !rate || !description || !mongoose.Types.ObjectId.isValid(userId) || !mongoose.Types.ObjectId.isValid(needId)) {
+        res.status(422);
+        res.json({ 'message': 'Unprocessable Entity' });
+        return;
+    }
+
+    const need = await Need.findById(needId);
+
+    if (!mongoose.Types.ObjectId(userId).equals(need.owner)) {
+        res.status = 403;
+        res.json({ 'message': 'Forbidden' });
+        return;
+    }
+
+    need.title = title;
+    need.rate = rate;
+    need.description = description;
+    need.save();
+
+    res.status(200);
+    res.json(need);
+    return;
 
 });
 
 module.exports = router;
+
+// { isActive: true,
+//     _id: 5c9d212b641885274272858f,
+//     owner: 5c9d074c9f38250f955ec0a7,
+//     title: 'test title',
+//     rate: 123,
+//     description: 'test desc',
+//     created_at: 2019-03-28T19:31:55.014Z,
+//     updated_at: 2019-03-28T19:31:55.014Z,
+//     __v: 0 }
+
+
+
+// { needId: '5c9e8688d140cc0e8ae0f120',
+//   title: '1233',
+//   rate: '123',
+//   description: '21331232321' }
+
